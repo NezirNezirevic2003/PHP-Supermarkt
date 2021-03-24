8b1974a002fffa132a7c6481779314b116b1b110
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2021-03-24 11:53:09 +0100

    commit

--- productZoeken.php
+++ productZoeken.php
@@ -14,12 +14,6 @@ include "./templates/header.php";
     <title>Document</title>
 </head>
 
-<?php
-if (isset($_POST['submit'])) {
-    $productnaam = $_POST['productzoekvak'];
-}
-?>
-
 <body>
     <div class="container">
         <form class="row g-3 mt-5" action="productZoeken.php" method="POST">
@@ -36,31 +30,29 @@ if (isset($_POST['submit'])) {
     <div class="container mb-4">
         <div class="row">
             <div class="col-md">
-                <div class="card mt-4" style="width: 15rem;">
-                    <?php
-                    if (isset($_POST['submit'])) {
-                        $productnaam = $_POST['productzoekvak'];
+                <?php
+                if (isset($_POST['submit'])) {
+                    $productnaam = $_POST['productzoekvak'];
 
-                        $producten = new Beheerder();
-                        foreach ($producten->zoekProduct($productnaam) as $product) { ?>
-                    <div class="container">
-                        <div style="margin-top: 20px">
-                            <h3>Zoekresultaten</h3>
-                            <hr class="my-4">
-                            </p>
-                        </div>
+                    $producten = new Beheerder();
+                    foreach ($producten->zoekProduct($productnaam) as $product) { ?>
+                <div class="container">
+                    <div style="margin-top: 20px">
+                        <h3>Zoekresultaten</h3>
+                        <hr class="my-4">
+                        </p>
                     </div>
-                    <?php echo "<img class='card-img-top src='embed src='data:" . $product['mime'] . ";base64," . base64_encode($product['data']) . "'width='200' height='100'"; ?>
-                    <-->
-                        <h5 class="card-title"><?php echo $product['productnaam']; ?></h5>
-                        <p class="card-text"><?php echo $product['productomschrijving'] ?></p>
-                        <a href="#" class="btn btn-primary">€ <?php echo $product['productprijs']; ?></a>
-                        <?php
-                        }
-                    }
-                        ?>
-
                 </div>
+                <?php echo "<img class='card-img-top src='embed src='data:" . $product['mime'] . ";base64," . base64_encode($product['data']) . "'width='200' height='520'"; ?>
+                <-->
+                    <h5 class="card-title"><?php echo $product['productnaam']; ?></h5>
+                    <p class="card-text"><?php echo $product['productomschrijving'] ?></p>
+                    <a href="#" class="btn btn-primary">€ <?php echo $product['productprijs']; ?></a>
+                    <?php
+                    }
+                }
+                    ?>
+
             </div>
 </body>
 
