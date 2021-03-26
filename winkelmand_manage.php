2d1c1dea5b6187a2a3ead0f33d2628cebc174aec
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2021-03-26 23:58:43 +0100

    commit

--- winkelmand_manage.php
+++ winkelmand_manage.php
@@ -7,21 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $producten = array_column($_SESSION['product'], 'productnaam');
 
             if (in_array($_POST['productnaam'], $producten)) {
-                echo "<script>alert('Product is al toegevoegd');
-                window.location.href='winkelmand.php';
-                </script>";
+                header('location: index.php');
             } else {
                 $count = count($_SESSION['product']);
                 $_SESSION['product'][$count] = array('productnaam' => $_POST['productnaam'], 'productomschrijving' => $_POST['productomschrijving'], 'productprijs' => $_POST['productprijs'], 'Quantity' => 1);
-                echo "<script>alert('Product is succesvol toegevoegd');
-            window.location.href='winkelmand.php';
-            </script>";
+                header('location: index.php');
             }
         } else {
             $_SESSION['product'][0] = array('productnaam' => $_POST['productnaam'], 'productomschrijving' => $_POST['productomschrijving'], 'productprijs' => $_POST['productprijs'], 'Quantity' => 1);
-            echo "<script>alert('Product is succesvol toegevoegd');
-            window.location.href='winkelmand.php';
-            </script>";
+            header('location: index.php');
         }
     }
     if (isset($_POST['verwijder'])) {
